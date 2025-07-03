d47bad365b9ef63868e30985ed342a047202c811
Juan Carlos Espinoza <112902085+juanespinoza94@users.noreply.github.com>
2025-07-02 23:58:16 -0500

    Revert "Agregado Item-Consumo"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,17 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
-
 // Interceptor
 import { TokenInterceptor } from './core/interceptors/token.interceptor';
 
 // Componentes
 import { AppComponent } from './app.component';
 import { RegistroComponent } from './features/huesped/registro/registro.component';
-import { ItemConsumoComponent } from './features/item-consumo/principal-item-consumo/principal-item-consumo.component'; 
-import { EmpleadoComponent } from './features/empleado/principal-empleado/principal-empleado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistroComponent,
-    ItemConsumoComponent,
-    EmpleadoComponent
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
